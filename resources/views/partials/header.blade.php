269eabf1262dd88656ec37f7a902830cc0634ac5
CraciDenisStefan <crdenis13@gmail.com>
2023-07-21 16:32:54 +0200

    aggiunto seach al header

--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -1,19 +1,17 @@
 
 <header>
     <div class="pre_header">
-    <div class="container">
-        dede
+        <div class="container d-flex justify-content-end">
+            dede
+        </div>
     </div>
-    </div>
     <div class="container">
-        <div class="row justify-content-between my-4 align-items-center">
-
+        <div class="row justify-content-between my-2 align-items-center">
             <div class="col">
                 <img src="{{Vite::asset('resources/img/dc-logo.png')}}">
             </div>
-
-            <div class="col d-flex justify-content-end">
-                <ul class=" menu_header list-group list-group-horizontal g-2">
+            <div class="col d-flex justify-content-end align-items-center">
+                <ul class=" menu_header list-group list-group-horizontal px-2">
                     <li><a :href="/">Characters</a></li>
                     <li><a :href="/">Comics</a></li>
                     <li><a :href="/">Movies</a></li>
@@ -25,14 +23,17 @@
                     <li><a :href="/">News</a></li>
                     <li><a :href="/">Shop</a></li>
                 </ul>
-                <div class="hamburgher_menu">
+                <div class="hamburgher_menu px-4">
                     <i class="fa-solid fa-bars"></i>
                 </div>
 
+                <div class="search_header border-bottom border border-0 border-primary ">
+                    <div class="d-flex ">
+                    <input type="text"  class="border-0 text-end" placeholder="Search">
+                    <i class="fa-solid fa-magnifying-glass mx-1"></i>
+                    </div>
+                </div>
             </div>
-    
         </div>
-
     </div>
-
 </header>
