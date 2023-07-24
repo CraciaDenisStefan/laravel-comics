7a7bc0a9d0dbb527d083c60d3bb99a043c1b4a36
CraciDenisStefan <crdenis13@gmail.com>
2023-07-24 15:12:56 +0200

    [FEAT]Copertina fumetto nel jumbo ok

--- resources/views/comics/show.blade.php
+++ resources/views/comics/show.blade.php
@@ -1,5 +1,19 @@
 @extends('layouts.app')
 
 @section('content')
+<div class="cover_img">
+    <!-- Jumbotron -->
+       <img src="{{Vite::asset('resources/img/jumbotron.jpg')}}" alt="">
+       <div class="down_jumbo">
+            <div class="container d-flex justify-content-center">
+                <div class="col-10">
+                    <div class="img_single">
+                        <img src="{{$comic['thumb']}}">
+                    </div> 
+                </div>      
+            </div>
+       </div>
+</div>
+
 {{$comic['series']}}
 @endsection
