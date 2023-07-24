7fafc04806158aac53136fecd0fbf1a78a0c2e69
CraciDenisStefan <crdenis13@gmail.com>
2023-07-24 18:38:08 +0200

    add commenti

--- resources/views/comics/show.blade.php
+++ resources/views/comics/show.blade.php
@@ -18,15 +18,18 @@
 <div class="container d-flex justify-content-center py-3">
     <div class="col-12 col-md-10">
         <div class="row">
+            {{-- Iizio parte iniziale con titolo del fumetto --}}
             <div class="col-12 col-lg-8 pt-5">
                 <h4 class="text-uppercase">
                     {{$comic['title']}}
                 </h4>
                 <div class="col-12 d-flex bg_price  fw-semibold text-white">
                     <div class="col-3 p-2">
+                        {{-- Prezzo del fumetto --}}
                        <span class="text_avaible"> U.S. PRICE :</span> {{$comic['price']}}
                     </div>
                     <div class="d-flex justify-content-end align-items-center col-9 ">
+                        {{-- Disponibilita --}}
                         <div class="text_avaible p-2 border-black border-end">
                             AVAIBLE
                         </div>
@@ -40,6 +43,7 @@
                 <p class="pt-3">{{$comic['description']}}</p>
             </div>
             <div class="col-12 col-lg-4 p-4">
+                {{-- Publicita --}}
                 <div class="text-end fw-semibold">
                     ADVERTISMENT
                 </div>
@@ -55,6 +59,7 @@
                 <div class="col-12 col-lg-6">
                     <h5 class="pb-3 fw-semibold">TALENT</h5>
             <hr>      
+                    {{-- Talent con dentro gli artisti e scrittori --}}
                     <div class="row justify-content-between">
                         <div class="col-4 fw-semibold">Art by : </div>
                         <div class="col-8">
@@ -79,6 +84,7 @@
 
                     <h5 class="pb-3 fw-semibold">Specs</h5>
                     <hr>      
+                            {{-- Specs con nome della serie prezzo e data --}}
                             <div class="row justify-content-between">
                                 <div class="col-4 fw-semibold">Series : </div>
                                 <div class="col-8">
@@ -111,6 +117,7 @@
 <div class="container_marketing">
     <div class="container d-flex justify-content-center">
         <div class="col-12 col-md-10">
+            {{-- Inizio parte commerciale  --}}
             <div class="row">
                 <div class="col-12 col-md-6 col-lg-3 d-flex justify-content-between border-dark-subtle border-end border-start p-3">
                     <div class="col-6 font_size fw-bolder text-secondary">DIGITAL COMICS</div>
