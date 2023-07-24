7c03677ad83521a2a2d771b578be15f1e19bdb3e
CraciDenisStefan <crdenis13@gmail.com>
2023-07-24 16:45:02 +0200

    [FEAT]Talent e specs ok, da sistemare data e qualche stile

--- resources/views/comics/show.blade.php
+++ resources/views/comics/show.blade.php
@@ -19,9 +19,9 @@
     <div class="col-12 col-md-10">
         <div class="row">
             <div class="col-12 col-lg-8 pt-5">
-                <h5 class="text-uppercase">
+                <h4 class="text-uppercase">
                     {{$comic['title']}}
-                </h5>
+                </h4>
                 <div class="col-12 d-flex bg_price  fw-semibold text-white">
                     <div class="col-3 p-2">
                        <span class="text_avaible"> U.S. PRICE :</span> {{$comic['price']}}
@@ -44,11 +44,65 @@
                     ADVERTISMENT
                 </div>
                 <img class="w_100" src="{{Vite::asset('resources/img/adv.jpg')}}" alt="">
-            </div>
-    
+            </div> 
         </div>
     </div>
 </div>
+<div class="container_down">
+    <div class="container d-flex justify-content-center">
+        <div class="col-12 col-md-10">
+            <div class="row">
+                <div class="col-12 col-lg-6">
+                    <h5 class="pb-3 fw-semibold">TALENT</h5>
+            <hr>      
+                    <div class="row justify-content-between">
+                        <div class="col-4 fw-semibold">Art by : </div>
+                        <div class="col-8">
+                            @foreach($comic['artists'] as $artist)
+                           <a class="text-decoration-none" href="#">{{$artist}}</a> ,
+                            @endforeach
+                        </div>
+                    </div>
+            <hr>
+                    <div class="row justify-content-between">
+                        <div class="col-4 fw-semibold">Writen by : </div>
+                        <div class="col-8">
+                            @foreach($comic['writers'] as $writer)
+                            <a class="text-decoration-none" href="#"> {{$writer}}</a> ,
+                            @endforeach
+                        </div>
+                    </div>
+            <hr>
+                </div>
 
-{{$comic['series']}}
+                <div class="col-12 col-lg-6">
+
+                    <h5 class="pb-3 fw-semibold">Specs</h5>
+                    <hr>      
+                            <div class="row justify-content-between">
+                                <div class="col-4 fw-semibold">Series : </div>
+                                <div class="col-8">
+                                   <a class="text-decoration-none" href="#">{{$comic['series']}}</a> 
+                                </div>
+                            </div>
+                    <hr>
+                            <div class="row justify-content-between">
+                                <div class="col-4 fw-semibold">U.S. Price : </div>
+                                <div class="col-8">
+                                    {{$comic['price']}} 
+                                </div>
+                            </div>
+                    <hr>
+                            <div class="row justify-content-between">
+                                <div class="col-4 fw-semibold">On Sale Date : </div>
+                                <div class="col-8">
+                                    {{$comic['sale_date']}}
+                                </div>
+                            </div>
+                    <hr>
+                        
+                </div>
+    </div>
+
+</div>
 @endsection
